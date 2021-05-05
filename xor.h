e5e577cbb296ddde2befe05c0225e5ebcbae9833
ja1l <38633228+ja1lman14@users.noreply.github.com>
2021-05-05 12:39:46 -0700

    Update xor.h

--- xor.h
+++ xor.h
@@ -3,8 +3,7 @@
 
 char* XOR(char* input, char key[]) {
     char* given_input = input;
-
-
+    
     for(int i = 0; i < strlen(input); i++) {
         given_input[i] = input[i] ^ key[(i % sizeof(key) / sizeof(char))];
     }
