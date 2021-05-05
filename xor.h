6e4317e89d4d4dde6d74bc36ec588676995cc60e
ja1l <38633228+ja1lman14@users.noreply.github.com>
2021-05-05 12:38:02 -0700

    Create xor.h

---
+++ xor.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+char* XOR(char* input, char key[]) {
+    char* given_input = input;
+
+
+    for(int i = 0; i < strlen(input); i++) {
+        given_input[i] = input[i] ^ key[(i % sizeof(key) / sizeof(char))];
+    }
+
+    return given_input;
+}
